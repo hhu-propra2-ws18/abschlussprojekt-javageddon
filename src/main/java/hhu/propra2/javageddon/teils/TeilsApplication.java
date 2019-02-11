80add0798ec815662d2d44f427388f6bfaeef1a1
Marvin Breyer <marvin.breyer@hhu.de>
2019-02-11 14:15:14 +0100

    Add SpringInitializer Files/Structure

---
+++ src/main/java/hhu/propra2/javageddon/teils/TeilsApplication.java
@@ -0,0 +1,14 @@
+package hhu.propra2.javageddon.teils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeilsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/hhu/propra2/javageddon/teils/TeilsApplicationTests.java
@@ -0,0 +1,17 @@
+package hhu.propra2.javageddon.teils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeilsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
