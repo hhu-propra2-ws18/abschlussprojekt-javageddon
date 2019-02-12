d105df72ac91b257610aaad06a8f723a90e729e4
Kurt <kurtulus.mermer@hhu.de>
2019-02-12 14:57:18 +0100

    add controller
    Co-authored-by: Jana0905 <jana.saemisch@t-online.de>

---
+++ src/main/java/hhu/propra2/javageddon/teils/web/ArtikelController.java
@@ -0,0 +1,46 @@
+package hhu.propra2.javageddon.teils.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ArtikelController {
+
+    @Value("${baseurl}")
+    private String baseurl;
+
+    @Autowired
+    private ArtikelRepository alleArtikel;
+
+    @GetMapping("/")
+    public String artikelListe(Model m){
+        m.addAttribute("baseurl", baseurl);
+        m.addAttribute("alleArtikel", alleArtikel.findByAktiv(true));
+        return "start";
+    }
+/*
+    @GetMapping("/edit/{id}")
+    public String editPerson(Model m, @PathVariable("id") int id) {
+        Person p = personen.getPerson(id);
+        m.addAttribute("baseurl", baseurl);
+        m.addAttribute("person", p);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String changePerson(Model m, Person p, String skillList) {
+        personen.merge(p, skillList);
+        return "redirect:" + baseurl;
+    }
+
+    @GetMapping("/add")
+    public String addPerson() {
+        return "redirect:" + baseurl + "/edit/" + personen.newPerson().getId();
+    } */
+}
