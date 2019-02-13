b392a93db81add2892903e29519c00b8e4b44db4
PGoettert <philipp.goettert@gmail.com>
2019-02-13 06:15:27 -0800

    Update Benutzer model to support testing via Builders
    Co-authored-by: FredFred14 <froeh100@uni-duesseldorf.de>

--- src/main/java/hhu/propra2/javageddon/teils/model/Benutzer.java
+++ src/main/java/hhu/propra2/javageddon/teils/model/Benutzer.java
@@ -1,6 +1,9 @@
 package hhu.propra2.javageddon.teils.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,9 @@ import javax.persistence.OneToMany;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Benutzer {
     @Id
