68a71dfb370f7ed7b45f82de523bbf743f004d20
Marvin Breyer <marvin.breyer@hhu.de>
2019-02-13 09:59:47 +0100

    Add ArtikelRepository
    Co-authored-by: flfis102 <flfis102@uni-duesseldorf.de>

---
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/ArtikelRepository.java
@@ -0,0 +1,9 @@
+package hhu.propra2.javageddon.teils.dataaccess;
+
+import hhu.propra2.javageddon.teils.model.Artikel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArtikelRepository extends CrudRepository<Artikel,Long> {
+
+
+}
