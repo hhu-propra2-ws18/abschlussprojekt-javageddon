bddb93562ec1393b045207bfd9bced45716d96f5
flfis102 <44257508+flfis102@users.noreply.github.com>
2019-02-15 13:37:43 +0100

    Add FotoStorage, FotoStorageInterface, UploadController, fotos_upload

---
+++ src/main/java/hhu/propra2/javageddon/teils/web/UploadController.java
@@ -0,0 +1,51 @@
+package hhu.propra2.javageddon.teils.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import hhu.propra2.javageddon.teils.dataaccess.FotoStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+public class UploadController {
+
+    @Autowired
+    FotoStorage fotoStorage;
+
+    @GetMapping("/")
+    public String index() {
+        return "uploadform";
+    }
+
+    @PostMapping("/")
+    public String uploadMultipartFile(@RequestParam("files") MultipartFile[] files, Model model) {
+        List<String> fileNames = null;
+
+        try {
+            fileNames = Arrays.asList(files)
+                    .stream()
+                    .map(file -> {
+                        fotoStorage.store(file);
+                        return file.getOriginalFilename();
+                    })
+                    .collect(Collectors.toList());
+
+            model.addAttribute("message", "Files uploaded successfully!");
+            model.addAttribute("files", fileNames);
+        } catch (Exception e) {
+            model.addAttribute("message", "Fail!");
+            model.addAttribute("files", fileNames);
+        }
+
+        return "uploadform";
+    }
+
+}
