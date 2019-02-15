202ae33e42c029224f32ff1a982aa2271f4f750b
FredFred14 <froeh100@uni-duesseldorf.de>
2019-02-15 14:35:47 +0100

    Add Repository for Reservierung in dataaccess
    Co-authored-by: PGoettert <philipp.goettert@gmail.com>

---
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/ReservierungRepository.java
@@ -0,0 +1,16 @@
+package hhu.propra2.javageddon.teils.dataaccess;
+
+import hhu.propra2.javageddon.teils.model.Artikel;
+import hhu.propra2.javageddon.teils.model.Benutzer;
+import org.springframework.data.repository.CrudRepository;
+import hhu.propra2.javageddon.teils.model.Reservierung;
+
+import java.util.List;
+
+public interface ReservierungRepository extends CrudRepository<Reservierung, Long> {
+
+    List<Reservierung> findByArtikel(Artikel a);
+    List<Reservierung> findByLeihender(Benutzer b);
+    List<Reservierung> findByArtikelAndLeihender(Artikel a, Benutzer b);
+
+}
