3a23dc2e650e5b3a3b5d57ee619da62248fd69a4
Marvin Breyer <marvin.breyer@hhu.de>
2019-02-15 13:08:53 +0100

    Update ArtikelController & Start.html Add VolumeMapping for the teils-DockerContainer
    Showing off Pictures Works, Refactor an further Tweaks requiered.
    
    Co-authored-by: flfis102 <flfis102@uni-duesseldorf.de>

--- src/main/java/hhu/propra2/javageddon/teils/web/ArtikelController.java
+++ src/main/java/hhu/propra2/javageddon/teils/web/ArtikelController.java
@@ -1,18 +1,30 @@
 package hhu.propra2.javageddon.teils.web;
 
-import java.util.List;
-import java.util.Optional;
-
 import hhu.propra2.javageddon.teils.dataaccess.ArtikelRepository;
-import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.context.support.ServletContextResource;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.JarURLConnection;
+import java.net.URL;
 
 @Controller
 public class ArtikelController {
+
+    private ResourceLoader resourceLoader = new DefaultResourceLoader();
+
 
     @Autowired
     private ArtikelRepository alleArtikel;
@@ -22,6 +34,16 @@ public class ArtikelController {
         m.addAttribute("alleArtikel", alleArtikel.findByAktiv(true));
         return "start";
     }
+
+    /*
+        Diese Methode greift auf das Dateisystem des Dockercontainers zu und liefert das angefragte Bild aus.
+     */
+    @ResponseBody
+    @RequestMapping(value = "/fotos/{id}", method = RequestMethod.GET)
+    public FileSystemResource getImageAsResource(@PathVariable("id") String id) {
+        return new FileSystemResource("fotos/" + id + ".jpg");
+    }
+
 /*
     @GetMapping("/edit/{id}")
     public String editPerson(Model m, @PathVariable("id") int id) {
