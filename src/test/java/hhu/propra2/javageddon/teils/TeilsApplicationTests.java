1e1f653ad26edb2c7e84f2351a4278ea1a78ec76
Marvin Breyer <marvin.breyer@hhu.de>
2019-02-15 14:11:15 +0100

    Add FailingTest to TeilsApplicationTest
    just for Fun!
    
    Co-authored-by: flfis102 <flfis102@uni-duesseldorf.de>

--- src/test/java/hhu/propra2/javageddon/teils/TeilsApplicationTests.java
+++ src/test/java/hhu/propra2/javageddon/teils/TeilsApplicationTests.java
@@ -4,13 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class TeilsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void thisTestHaveToFail(){
+		assertThat(false).isEqualTo(true);
 	}
 
 }
