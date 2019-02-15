ef9c126ac6da74f50a0213560657599e1a6925e1
flfis102 <44257508+flfis102@users.noreply.github.com>
2019-02-15 14:12:51 +0100

    Update FotoStorage, FotoStorageInterface, fotos_upload

--- src/main/java/hhu/propra2/javageddon/teils/dataaccess/FotoStorage.java
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/FotoStorage.java
@@ -1,25 +1,15 @@
 package hhu.propra2.javageddon.teils.dataaccess;
 
-import java.io.IOException;
-        import java.net.MalformedURLException;
         import java.nio.file.Files;
         import java.nio.file.Path;
         import java.nio.file.Paths;
         import java.nio.file.StandardCopyOption;
-        import java.util.stream.Stream;
-
-        import org.slf4j.Logger;
-        import org.slf4j.LoggerFactory;
-        import org.springframework.core.io.Resource;
-        import org.springframework.core.io.UrlResource;
         import org.springframework.stereotype.Service;
-        import org.springframework.util.FileSystemUtils;
         import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FotoStorage implements FotoStorageInterface{
 
-    Logger log = LoggerFactory.getLogger(this.getClass().getName());
     private final Path rootLocation = Paths.get("fotos");
 
     @Override
@@ -30,47 +20,4 @@ public class FotoStorage implements FotoStorageInterface{
             throw new RuntimeException("FAIL! -> message = " + e.getMessage());
         }
     }
-/*
-    @Override
-    public Resource loadFile(String filename) {
-        try {
-            Path file = rootLocation.resolve(filename);
-            Resource resource = new UrlResource(file.toUri());
-            if(resource.exists() || resource.isReadable()) {
-                return resource;
-            }else{
-                throw new RuntimeException("FAIL!");
-            }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Error! -> message = " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void deleteAll() {
-        FileSystemUtils.deleteRecursively(rootLocation.toFile());
-    }
-
-    @Override
-    public void init() {
-        try {
-            Files.createDirectory(rootLocation);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not initialize storage!");
-        }
-    }
-
-    @Override
-    public Stream<Path> loadFiles() {
-        try {
-            return Files.walk(this.rootLocation, 1)
-                    .filter(path -> !path.equals(this.rootLocation))
-                    .map(this.rootLocation::relativize);
-        }
-        catch (IOException e) {
-            throw new RuntimeException("\"Failed to read stored file");
-        }
-    }
-
-    */
 }
--- src/main/java/hhu/propra2/javageddon/teils/dataaccess/FotoStorageInterface.java
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/FotoStorageInterface.java
@@ -1,15 +1,7 @@
 package hhu.propra2.javageddon.teils.dataaccess;
 
-import java.nio.file.Path;
-import java.util.stream.Stream;
-
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FotoStorageInterface {
     public void store(MultipartFile file);
-//    public Resource loadFile(String filename);
-//    public void deleteAll();
-//    public void init();
-//    public Stream<Path> loadFiles();
 }
