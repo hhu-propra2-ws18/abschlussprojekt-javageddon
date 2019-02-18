60c12d19d573f450a6b333b49c34e1a7d2956cea
flfis102 <44257508+flfis102@users.noreply.github.com>
2019-02-18 13:35:34 +0100

    Update FotoStorage.java

--- src/main/java/hhu/propra2/javageddon/teils/dataaccess/FotoStorage.java
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/FotoStorage.java
@@ -4,7 +4,9 @@ package hhu.propra2.javageddon.teils.dataaccess;
         import java.nio.file.Path;
         import java.nio.file.Paths;
         import java.nio.file.StandardCopyOption;
+        import java.util.List;
 
+        import hhu.propra2.javageddon.teils.model.Foto;
         import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Service;
         import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +15,7 @@ package hhu.propra2.javageddon.teils.dataaccess;
 public class FotoStorage implements FotoStorageInterface{
 
     private final Path rootLocation = Paths.get("fotos");
+    private int index = 0;
 
     @Autowired
     FotoRepository fotoRepository;
@@ -20,7 +23,10 @@ public class FotoStorage implements FotoStorageInterface{
     @Override
     public void store(MultipartFile file){
         try {
-            Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+            String newName = "" + index;
+            Files.copy(file.getInputStream(), this.rootLocation.resolve(newName), StandardCopyOption.REPLACE_EXISTING);
+            //Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+            index++;
         } catch (Exception e) {
             throw new RuntimeException("FAIL! -> message = " + e.getMessage());
         }
