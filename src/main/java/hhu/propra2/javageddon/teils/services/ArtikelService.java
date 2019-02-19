e48024c24d8e202e3c25f1f598507671c5493d03
FredFred14 <froeh100@uni-duesseldorf.de>
2019-02-19 13:52:06 +0100

    Add method for finding Artikel by id
    Co-authored-by: PGoettert <philipp.goettert@gmail.com>

--- src/main/java/hhu/propra2/javageddon/teils/services/ArtikelService.java
+++ src/main/java/hhu/propra2/javageddon/teils/services/ArtikelService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import hhu.propra2.javageddon.teils.dataaccess.ArtikelRepository;
 import hhu.propra2.javageddon.teils.model.Artikel;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ArtikelService {
 
 	private ArtikelRepository alleArtikel;
