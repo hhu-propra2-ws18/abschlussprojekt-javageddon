95d639e6519e6d9454d9c9e295deb7bd53a2a0cc
Marvin Breyer <marvin.breyer@hhu.de>
2019-02-19 21:38:52 +0100

    MAKE IT WORKING

--- src/main/java/hhu/propra2/javageddon/teils/web/WebSecurityConfig.java
+++ src/main/java/hhu/propra2/javageddon/teils/web/WebSecurityConfig.java
@@ -1,18 +1,41 @@
 package hhu.propra2.javageddon.teils.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.activation.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(jdbcTemplate.getDataSource())
+                .usersByUsernameQuery(
+                        "select name,password, true from benutzer where name=?")
+                .authoritiesByUsernameQuery(
+                        "select name, 'ROLE_USER' from benutzer where name=?");
+        //TODO NutzerRolle ordentlich implementieren
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -30,14 +53,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public JdbcUserDetailsManager userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
+        manager.setJdbcTemplate(jdbcTemplate);
+        return manager;
+    }
+//Passwörter werden PLAIN in der DB abgelegt, nicht schön aber simpel.
+    //TODO bei ganz viel Langeweile ordentlich implementieren
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 }
