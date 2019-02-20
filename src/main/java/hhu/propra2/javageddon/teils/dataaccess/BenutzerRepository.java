616b0b016cfc6587527327317d0eaef26187bc14
Kurt <kurtulus.mermer@hhu.de>
2019-02-20 10:50:21 +0100

    Update BenutzerRepository
    Co-authored-by: Jana0905 <jana.saemisch@t-online.de>

--- src/main/java/hhu/propra2/javageddon/teils/dataaccess/BenutzerRepository.java
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/BenutzerRepository.java
@@ -2,11 +2,10 @@ package hhu.propra2.javageddon.teils.dataaccess;
 
 import hhu.propra2.javageddon.teils.model.Benutzer;
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 
 public interface BenutzerRepository extends CrudRepository<Benutzer,Long> {
 
-    List<Benutzer> findByEmail(String email);
+    Benutzer findById(long i);
 
     boolean existsByEmail(String email);
     boolean existsByName(String name);
