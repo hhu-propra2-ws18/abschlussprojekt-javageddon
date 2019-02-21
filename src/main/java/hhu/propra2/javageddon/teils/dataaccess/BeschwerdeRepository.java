d41ccf2bf9daeb3a543fad0c2f74eb69f91be416
Marvin Breyer <marvin.breyer@hhu.de>
2019-02-21 14:00:48 +0100

    Add Repository for Beschwerde
    Co-authored-by: flfis102<flfis102hhu.de>

---
+++ src/main/java/hhu/propra2/javageddon/teils/dataaccess/BeschwerdeRepository.java
@@ -0,0 +1,14 @@
+package hhu.propra2.javageddon.teils.dataaccess;
+
+import hhu.propra2.javageddon.teils.model.Artikel;
+import hhu.propra2.javageddon.teils.model.Benutzer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BeschwerdeRepository extends CrudRepository<Artikel,Long> {
+
+    List<Artikel> findByAktiv(boolean b);
+    Artikel findById(long i);
+
+}
