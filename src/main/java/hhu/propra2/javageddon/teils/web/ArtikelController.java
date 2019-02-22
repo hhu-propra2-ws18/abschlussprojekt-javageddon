56aacebbf9b47016c8822ada3ea7a17eab0d3a99
flfis102 <44257508+flfis102@users.noreply.github.com>
2019-02-22 14:46:20 +0100

    Update ArtikelController.java
    Co-Authored-By: pgoettert <pgoettert@users.noreply.github.com>

--- src/main/java/hhu/propra2/javageddon/teils/web/ArtikelController.java
+++ src/main/java/hhu/propra2/javageddon/teils/web/ArtikelController.java
@@ -3,12 +3,9 @@ package hhu.propra2.javageddon.teils.web;
 import hhu.propra2.javageddon.teils.dataaccess.ArtikelRepository;
 import hhu.propra2.javageddon.teils.dataaccess.BenutzerRepository;
 import hhu.propra2.javageddon.teils.dataaccess.BeschwerdeRepository;
-import hhu.propra2.javageddon.teils.model.Artikel;
-import hhu.propra2.javageddon.teils.model.Benutzer;
-import hhu.propra2.javageddon.teils.model.Reservierung;
+import hhu.propra2.javageddon.teils.model.*;
 import hhu.propra2.javageddon.teils.services.ArtikelService;
 import hhu.propra2.javageddon.teils.services.BenutzerService;
-import hhu.propra2.javageddon.teils.model.Adresse;
 import hhu.propra2.javageddon.teils.services.ReservierungService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
@@ -84,6 +81,25 @@ public class ArtikelController {
         artikel.setEigentuemer(alleBenutzer.findBenutzerById(id));
         alleArtikel.addArtikel(artikel);
         return "redirect:/fotoupload/" + artikel.getId();
+    }
+
+    @RequestMapping(value = "/beschweren", method = GET)
+    public String artikelReservieren(Model m, @RequestParam("id") long id, @ModelAttribute Reservierung reservierung){
+        m.addAttribute("reservierung",reservierung);
+        return "artikel_reservieren";
+    }
+
+    @PostMapping("/beschweren")
+    public String reserviereArtikel(@ModelAttribute Reservierung reservierung){
+        Beschwerde beschwerde = new Beschwerde();
+        Object currentUser = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((UserDetails)currentUser).getUsername();
+        Long id = alleBenutzer.getIdByName(username);
+        beschwerde.setReservierung(reservierung);
+        beschwerde.setBearbeitet(false);
+        beschwerde.setNutzer(reservierung.getLeihender());
+        
+        return "redirect:/";
     }
 
 }
