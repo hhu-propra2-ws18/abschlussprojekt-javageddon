ada4da55294cef14b575e51d3fd83cf727828397
flfis102 <44257508+flfis102@users.noreply.github.com>
2019-02-27 09:55:02 +0100

    Add Transaktionen

---
+++ src/main/java/hhu/propra2/javageddon/teils/model/Transaktion.java
@@ -0,0 +1,32 @@
+package hhu.propra2.javageddon.teils.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Transaktion {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    private Benutzer kontoinhaber;
+
+    private LocalDate datum;
+
+    private String verwendungszweck;
+    
+    private int betrag;
+}
