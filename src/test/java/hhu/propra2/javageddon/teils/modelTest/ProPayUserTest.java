fa04c8ef785e8bbe0028ec7456df23134b0f1922
flfis102 <44257508+flfis102@users.noreply.github.com>
2019-03-01 14:02:25 +0100

    Update ProPayUserTest.java
    Co-Authored-By: pgoettert <pgoettert@users.noreply.github.com>

--- src/test/java/hhu/propra2/javageddon/teils/modelTest/ProPayUserTest.java
+++ src/test/java/hhu/propra2/javageddon/teils/modelTest/ProPayUserTest.java
@@ -4,6 +4,8 @@ import hhu.propra2.javageddon.teils.model.ProPayUser;
 import hhu.propra2.javageddon.teils.model.Reservations;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProPayUserTest {
@@ -11,18 +13,18 @@ public class ProPayUserTest {
     Reservations reservations1 = new Reservations().builder().amount(50).id(1).build();
     Reservations reservations2 = new Reservations().builder().amount(30).id(2).build();
 
-    ProPayUser user1 = new ProPayUser().builder().amount(100).build();
+    ProPayUser user = new ProPayUser().builder().amount(100).account("user").reservations(new ArrayList<>()).build();
 
     @Test
     public void fullAmountNoReservations(){
-        assertThat(user1.getVerfuegbaresGuthaben()).isEqualTo(100);
+        assertThat(user.getVerfuegbaresGuthaben()).isEqualTo(100);
     }
 
     @Test
     public void correctAmountTwoReservations(){
 
-        user1.addReservation(reservations1);
-        user1.addReservation(reservations2);
-        assertThat(user1.getVerfuegbaresGuthaben()).isEqualTo(20);
+        user.addReservation(reservations1);
+        user.addReservation(reservations2);
+        assertThat(user.getVerfuegbaresGuthaben()).isEqualTo(20);
     }
 }
