8782cf73ff7e35e05d007299a5bd6ba3d105b32f
Marvin Breyer <marvin.breyer@hhu.de>
2019-03-01 10:49:57 +0100

    REFACTOR Upload Controller
    Co-authored-by: PGoettert <philipp.goettert@gmail.com>

--- src/main/java/hhu/propra2/javageddon/teils/web/UploadController.java
+++ src/main/java/hhu/propra2/javageddon/teils/web/UploadController.java
@@ -10,7 +10,7 @@ import hhu.propra2.javageddon.teils.dataaccess.FotoStorage;
 import hhu.propra2.javageddon.teils.dataaccess.VerkaufArtikelRepository;
 import hhu.propra2.javageddon.teils.model.Artikel;
 import hhu.propra2.javageddon.teils.model.VerkaufArtikel;
-import hhu.propra2.javageddon.teils.model.Verkauf;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,33 +40,7 @@ public class UploadController {
 
     @PostMapping("/fotoupload/{id}")
     public String uploadMultipartFile(@RequestParam("files") MultipartFile[] files, Model model, @PathVariable long id) {
-        List<String> fileNames = null;
-        ArrayList<String> renamedFiles = new ArrayList<String>();
-
-        try {
-             if(files.length < 11) {        //TODO Make it Variable
-                                            // TODO Check for JPG
-                 fileNames = Arrays.asList(files)
-                         .stream()
-                         .map(file -> {
-                             String newName = fotoStorage.store(file);
-                             if(newName.equals("nichts") == false) {
-                                 renamedFiles.add(newName);
-                             }
-                             return file.getOriginalFilename();
-                         })
-                         .collect(Collectors.toList());
-                 model.addAttribute("message", "Fotos uploaded successfully!");
-                 model.addAttribute("files", fileNames);
-             } else{
-                 model.addAttribute("message", "Zu viele Fotos!");
-                 model.addAttribute("files", fileNames);
-                    }
-        } catch (Exception e) {
-            model.addAttribute("message", "Something went wrong!");
-            model.addAttribute("files", fileNames);
-        }
-
+        ArrayList<String> renamedFiles = fotoSpeichern(files, model);
         Artikel artikel = alleArtikel.findById(id);
         artikel.setFotos(renamedFiles);
         alleArtikel.save(artikel);
@@ -81,6 +55,15 @@ public class UploadController {
 
     @PostMapping("/verkauf/fotoupload/{id}")
     public String uploadMultipartFileVerkauf(@RequestParam("files") MultipartFile[] files, Model model, @PathVariable long id) {
+        ArrayList<String> renamedFiles = fotoSpeichern(files, model);
+        VerkaufArtikel artikel = alleVerkaufArtikel.findById(id);
+        artikel.setFotos(renamedFiles);
+        alleVerkaufArtikel.save(artikel);
+
+        return "redirect:/verkauf/details/?id=" + id;
+    }
+
+    private ArrayList<String> fotoSpeichern(@RequestParam("files") MultipartFile[] files, Model model) {
         List<String> fileNames = null;
         ArrayList<String> renamedFiles = new ArrayList<String>();
 
@@ -107,12 +90,7 @@ public class UploadController {
             model.addAttribute("message", "Something went wrong!");
             model.addAttribute("files", fileNames);
         }
-
-        VerkaufArtikel artikel = alleVerkaufArtikel.findById(id);
-        artikel.setFotos(renamedFiles);
-        alleVerkaufArtikel.save(artikel);
-
-        return "redirect:/verkauf/details/?id=" + id;
+        return renamedFiles;
     }
 
 
