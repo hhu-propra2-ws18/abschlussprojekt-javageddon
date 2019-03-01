3ec9be22fb19fa5d05fcd4ba71b4055146511ed7
Marvin Breyer <marvin.breyer@hhu.de>
2019-03-01 11:24:53 +0100

    REFACTOR ProPayService
    Co-authored-by: PGoettert <philipp.goettert@gmail.com>

--- src/main/java/hhu/propra2/javageddon/teils/services/ProPayService.java
+++ src/main/java/hhu/propra2/javageddon/teils/services/ProPayService.java
@@ -1,7 +1,6 @@
 package hhu.propra2.javageddon.teils.services;
 
 import hhu.propra2.javageddon.teils.model.*;
-import org.springframework.http.HttpEntity;
 import hhu.propra2.javageddon.teils.model.Aufladung;
 import hhu.propra2.javageddon.teils.model.ProPayUser;
 import org.springframework.web.client.RestTemplate;
@@ -14,33 +13,23 @@ public class ProPayService {
     static final String URL_ACCOUNT = "http://proPay:8888/account/";
     static final String URL_RESERVATION = "http://proPay:8888/reservation/";
 
-        public static ProPayUser getProPayUser(String username) {
-
-            RestTemplate restTemplate = new RestTemplate();
-
-            // Send request with GET method and default Headers.
-            return  restTemplate.getForObject(URL_ACCOUNT + username, ProPayUser.class);
-
-        }
-
-        public static void heresTheMoney(Aufladung aufladung){
-
-            RestTemplate restTemplate = new RestTemplate();
-            restTemplate.postForObject(URL_ACCOUNT + aufladung.getProPayUser().getAccount() + "?amount=" +(int)aufladung.getBetrag(),"amount="+ (int)aufladung.getBetrag(),ProPayUser.class);
-        }
-
-    public static void executeTransaktion(Ueberweisung ueberweisung){
+    public static ProPayUser getProPayUser(String username) {
 
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(URL_ACCOUNT + ueberweisung.getKontoinhaber().getAccount()+ "/transfer/" +
-                                    ueberweisung.getZielKonto().getAccount() + "?amount=" + ueberweisung.getBetrag(),
-                                    "amount="+ (int)ueberweisung.getBetrag(),ProPayUser.class);
+
+        // Send request with GET method and default Headers.
+        return  restTemplate.getForObject(URL_ACCOUNT + username, ProPayUser.class);
+
     }
 
+    public static void heresTheMoney(Aufladung aufladung){
 
-
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(URL_ACCOUNT + aufladung.getProPayUser().getAccount() + "?amount=" +(int)aufladung.getBetrag(),"amount="+ (int)aufladung.getBetrag(),ProPayUser.class);
+    }
 
     public static Reservations executeReservation(Reservations reservations, Benutzer zielKonto, Benutzer kontoinhaber){
+
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.postForObject(URL_RESERVATION + "reserve/" + kontoinhaber.getName()+ "/" +
                                         zielKonto.getName() + "?amount=" + reservations.getAmount(),
